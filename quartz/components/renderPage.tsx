dad15e6f7e93c87b500c9a6a94cff2e2ed28b450
Magdy Samir <theparadox@Magdys-MacBook-Pro.local>
2026-08-02 01:44:37 +0300

    Prevent small text from shrinking column

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -33,13 +33,13 @@ export function pageResources(
   const resources: StaticResources = {
     css: [
       {
-        content: joinSegments(baseDir, "index.css") + "?v=20260802-7",
+        content: joinSegments(baseDir, "index.css") + "?v=20260802-8",
       },
       ...staticResources.css,
     ],
     js: [
       {
-        src: joinSegments(baseDir, "prescript.js") + "?v=20260802-7",
+        src: joinSegments(baseDir, "prescript.js") + "?v=20260802-8",
         loadTime: "beforeDOMReady",
         contentType: "external",
       },
@@ -55,7 +55,7 @@ export function pageResources(
   }
 
   resources.js.push({
-    src: joinSegments(baseDir, "postscript.js") + "?v=20260802-7",
+    src: joinSegments(baseDir, "postscript.js") + "?v=20260802-8",
     loadTime: "afterDOMReady",
     moduleType: "module",
     contentType: "external",
